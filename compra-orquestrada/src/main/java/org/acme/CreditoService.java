cb7477dc73a6c066af7755bd4052c6867afdef1f
Henio Junior <henioajunior@gmail.com>
2024-08-12 15:58:13 -0300

    Criação dos services

---
+++ compra-orquestrada/src/main/java/org/acme/CreditoService.java
@@ -0,0 +1,28 @@
+package org.acme;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CreditoService {
+
+    private int creditoTotal;
+    private Map<Long, Integer> pedidoValor = new HashMap<>();
+
+    public CreditoService() {
+        this.creditoTotal = 100;
+    }
+
+    public void newPedidoValor(Long pedidoId, int valor) {
+        if(valor > creditoTotal) {
+            throw new IllegalStateException("Saldo insuficiente");
+        }
+
+        creditoTotal -= valor;
+        pedidoValor.put(pedidoId, valor);
+    }
+
+    public void cancelPedidoValor(Long id) {
+        creditoTotal += pedidoValor.get(id);
+        pedidoValor.remove(id);
+    }
+}
